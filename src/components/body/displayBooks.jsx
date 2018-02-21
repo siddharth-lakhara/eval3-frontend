ca37d6eacdd1942b03e875a74189e794e835e6d7
lakhara <siddharth.lakhara@gmail.com>
2018-02-21 17:09:27 +0530

    display card corrected

--- src/components/body/displayBooks.jsx
+++ src/components/body/displayBooks.jsx
@@ -5,18 +5,28 @@ import BookCard from './bookCard';
 class DisplayBooks extends Component {
   render() {
     const bookObject = this.props.booksStorage;
-    for (const authorProp in bookObject) {
-      for (const bookElem in bookObject[authorProp]) {
-        return (<BookCard
-          Name={bookObject[authorProp][bookElem].Name}
-          rating={bookObject[authorProp][bookElem].rating}
+    return Object.keys(bookObject).map(authorProp =>
+      Object.keys(bookObject[authorProp]).map(BookElem => (
+        <BookCard
+          Name={bookObject[authorProp][BookElem].Name}
+          rating={bookObject[authorProp][BookElem].rating}
           // like
-          // like={bookObject.authorProp[bookElem].like}
+          // like={bookObject.authorProp[BookElem].like}
           author={authorProp}
         />
-        );
-      }
-    }
+      )));
+    // for (const authorProp in bookObject) {
+    //   for (const bookElem in bookObject[authorProp]) {
+    //     return (<BookCard
+    //       Name={bookObject[authorProp][bookElem].Name}
+    //       rating={bookObject[authorProp][bookElem].rating}
+    //       // like
+    //       // like={bookObject.authorProp[bookElem].like}
+    //       author={authorProp}
+    //     />
+    //     );
+    //   }
+    // }
   }
 }
 
